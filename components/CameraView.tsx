83d30a9ad5dfd17b0af6df763b0c270fb34a6395
bdjohnson529 <bdjohnson529@gmail.com>
2025-07-26 00:07:09 +0100

    wip: add frameprocessor boilerplate code

--- components/CameraView.tsx
+++ components/CameraView.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { useTensorflowModel } from 'react-native-fast-tflite';
-import { CameraDevice, Camera as VisionCamera } from 'react-native-vision-camera';
+import { CameraDevice, Camera as VisionCamera, useFrameProcessor } from 'react-native-vision-camera';
 
+import { useResizePlugin } from 'vision-camera-resize-plugin';
 
 interface CameraViewProps {
   device: CameraDevice | undefined;
@@ -13,11 +14,28 @@ interface CameraViewProps {
 function CameraWithModel({ device, isActive, style }: CameraViewProps) {
   const model = useTensorflowModel(require('../assets/models/1.tflite'));
 
+  const { resize } = useResizePlugin();
+  const frameProcessor = useFrameProcessor((frame) => {
+      'worklet'
+
+      const resized = resize(frame, {
+        scale: {
+          width: 192,
+          height: 192
+        },
+        pixelFormat: 'rgb',
+        dataType: 'uint8'
+      });
+
+    // Use resized data with your TensorFlow model here
+    }, [resize]);
+
   return (
     <VisionCamera
       style={[StyleSheet.absoluteFill, style]}
       device={device!}
       isActive={isActive}
+      frameProcessor={frameProcessor}
     />
   );
 }
