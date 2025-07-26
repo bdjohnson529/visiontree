ba3b8fdeb87db4e030140561a98631bfd1fd29bb
bdjohnson529 <bdjohnson529@gmail.com>
2025-07-26 19:12:44 +0100

    feat: add useFocusEffect to toggle camera between tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,22 @@
 import { StyleSheet } from 'react-native';
 import { useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback, useState } from 'react';
 
 import PermissionScreen from '@/components/PermissionScreen';
 import CameraView from '@/components/CameraView';
 
 export default function HomeScreen() {
   const { hasPermission, requestPermission } = useCameraPermission();
-  const device = useCameraDevice('back')
+  const device = useCameraDevice('back');
+  const [isFocused, setIsFocused] = useState(false);
+
+  useFocusEffect(
+    useCallback(() => {
+      setIsFocused(true);
+      return () => setIsFocused(false);
+    }, [])
+  );
 
   if (!hasPermission) {
     return <PermissionScreen onPress={requestPermission} />
@@ -15,7 +25,7 @@ export default function HomeScreen() {
       <CameraView
         style={StyleSheet.absoluteFill}
         device={device}
-        isActive={true}
+        isActive={isFocused}
       />
     );
   }
